e7258982f72bb01867a61795cbe8faf94099d67b
Chralu <contact@charly-caulet.net>
2025-09-09 14:55:34 +0300

    feat(config): disable vpn setup

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -51,7 +51,7 @@
         ./mosh.nix
         ./firewall.nix
         # ./dns.nix
-        ./vpn.nix
+        # ./vpn.nix
         ./users.nix
         ./virtualisation.nix
         ./programming-languages.nix
