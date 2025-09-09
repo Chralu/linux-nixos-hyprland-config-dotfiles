bc60fc2dcf2a25e47fe8a981f9dd39f304c1ce6e
Chralu <contact@charly-caulet.net>
2025-09-09 14:44:14 +0300

    feat(users): update applications

--- nixos/users.nix
+++ nixos/users.nix
@@ -2,18 +2,14 @@
 
 {
   # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.xnm = {
+  users.users.chralu = {
     isNormalUser = true;
     description = "xnm";
     extraGroups = [ "networkmanager" "input" "wheel" "video" "audio" "tss" ];
     shell = pkgs.fish;
     packages = with pkgs; [
-      spotify
-      youtube-music
-      discord
-      tdesktop
       vscodium
-      brave
+      firefox
     ];
   };
 
