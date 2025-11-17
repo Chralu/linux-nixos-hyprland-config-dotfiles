496e2e4f145c0f51740d42712369e48751c0424e
Chralu <contact@charly-caulet.net>
2025-11-17 12:10:53 +0100

    feat(programs): remove cattpuccingtk which is not maintained anymore

--- theme.nix
+++ theme.nix
@@ -2,14 +2,6 @@
 
 {
   # Enable Theme
-  environment.variables.GTK_THEME = "catppuccin-macchiato-teal-standard";
-  environment.variables.XCURSOR_THEME = "Catppuccin-Macchiato-Teal";
-  environment.variables.XCURSOR_SIZE = "24";
-  environment.variables.HYPRCURSOR_THEME = "Catppuccin-Macchiato-Teal";
-  environment.variables.HYPRCURSOR_SIZE = "24";
-  qt.enable = true;
-  # qt.platformTheme = "gtk2";
-  # qt.style = "gtk2";
   console = {
     earlySetup = true;
     colors = [
@@ -32,25 +24,9 @@
     ];
   };
 
-  # Override packages
-  nixpkgs.config.packageOverrides = pkgs: {
-    colloid-icon-theme = pkgs.colloid-icon-theme.override { colorVariants = ["teal"]; };
-    catppuccin-gtk = pkgs.catppuccin-gtk.override {
-      accents = [ "teal" ]; # You can specify multiple accents here to output multiple themes 
-      size = "standard";
-      variant = "macchiato";
-    };
-    # discord = pkgs.discord.override {
-      # withOpenASAR = true;
-      # withTTS = true;
-    # };
-  };
-
   environment.systemPackages = with pkgs; [
-    numix-icon-theme-circle
-    colloid-icon-theme
-    catppuccin-gtk
     catppuccin-kvantum
+    catppuccin-kde
     catppuccin-cursors.macchiatoTeal
 
     # gnome.gnome-tweaks
